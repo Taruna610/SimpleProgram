5a703bd81547adadbfd9c1e0f0e5db51e2513f1e
Taruna610 <arorataruna610@gmail.com>
2017-08-27 15:03:28 +0530

    Commiting project

---
+++ src/main/java/com/java/examples/DateExample.java
@@ -0,0 +1,20 @@
+package com.java.examples;
+import java.util.Date;
+
+public class DateExample {
+
+
+
+  public static void main(String[] args) {
+   
+    /*
+     To create a Date object for sepefic date and time use
+     Date(long date) constuctor where date is the number of milliseconds
+     since January 1, 1970, 00:00:00 GMT.
+    */
+   
+    //date after one year of January 1, 1970, 00:00:00 GMT
+    Date d = new Date(365L * 24L * 60L * 60L * 1000L);
+    System.out.println(d);
+  }
+}
